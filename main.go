88b53f9204bedb0a1a1064d3e8d51fd6e10e2f57
Brayden Killeen <me@braydenkilleen.com>
2021-01-16 17:22:03 +1100

    extract intitialisation to separate function

--- main.go
+++ main.go
@@ -1,48 +1,58 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/braydenkilleen/ril/database"
-	"github.com/braydenkilleen/ril/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"honnef.co/go/tools/config"
 )
 
-func initDatabase() {
-	var err error
-	database.DBConn, err = gorm.Open("sqlite3", "books.db")
-	if err != nil {
-		panic("failed to connect database")
+// func initDatabase() {
+// 	var err error
+// 	database.DB, err = gorm.Open("sqlite3", "bookmarks.db")
+// 	if err != nil {
+// 		panic("failed to connect database")
+// 	}
+// 	fmt.Println("Connected to database.")
+// 	database.DB.AutoMigrate(&model.Bookmark{})
+// 	fmt.Println("Database Migrated")
+// }
+
+// func setupRoutes(app *fiber.App) {
+// 	//
+
+// 	app.Get("/api/v1/bookmarks", model.GetBookmarks)
+// 	app.Get("/api/v1/bookmarks/:id", model.GetBookmark)
+// 	app.Post("/api/v1/bookmarks", model.NewBookmark)
+// 	app.Delete("/api/v1/bookmarks/:id", model.DeleteBookmark)
+
+// 	// 404 Handler
+// 	app.Use(func(c *fiber.Ctx) error {
+// 		return c.SendStatus(404)
+// 	})
+// }
+
+// Setup ...
+func Setup() *fiber.App {
+	if err := config.Load(); err != nil {
+		log.Fatalf("Couldn't load configuration file: %v", err)
 	}
-	fmt.Println("Connected to database.")
-	database.DBConn.AutoMigrate(&models.Bookmark{})
-	fmt.Println("Database Migrated")
-}
 
-func setupRoutes(app *fiber.App) {
-	app.Get("/api/v1/bookmarks", models.GetBookmarks)
-	app.Get("/api/v1/bookmarks/:id", models.GetBookmark)
-	app.Post("/api/v1/bookmarks", models.NewBookmark)
-	app.Delete("/api/v1/bookmarks/:id", models.DeleteBookmark)
-
-	// 404 Handler
-	app.Use(func(c *fiber.Ctx) error {
-		return c.SendStatus(404)
-	})
+	database.Init()
+	return server.Start()
 }
 
 func main() {
 	app := fiber.New()
 	app.Use(cors.New())
 
-	initDatabase()
-	defer database.DBConn.Close()
+	database.Connect()
+	// initDatabase()
+	// defer database.DBConn.Close()
 
-	setupRoutes(app)
-
-	log.Fatal(app.Listen(":3000"))
+	// setupRoutes(app)
+	// log.Fatal(app.Listen(":3000"))
 }
